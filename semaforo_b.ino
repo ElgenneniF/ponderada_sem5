a7a25fd9e3021c716ad842c093d62a89d7693ee5
ElgenneniF <159142546+ElgenneniF@users.noreply.github.com>
2024-11-22 01:36:33 -0300

    Update semaforo_b.ino

--- semaforo_b.ino
+++ semaforo_b.ino
@@ -1,12 +1,12 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
-#define LED_R 21
-#define LED_Y 19
-#define LED_G 18
+#define LED_R 13
+#define LED_Y 12
+#define LED_G 14
 
-const char* SSID = "SHARE-RESIDENTE";
-const char* PASSWORD = "Share@residente23";
+const char* SSID = "";
+const char* PASSWORD = "";
 
 const char* MQTT_SERVER = "industrial.api.ubidots.com";
 const char* MQTT_CLIENT_ID = "esp32_t12_g02_2";
@@ -18,8 +18,12 @@ PubSubClient client(espClient);
 unsigned long ultimoReconnectWiFi = 0;
 unsigned long ultimoReconnectMQTT = 0;
 unsigned long ultimoPiscarAmarelo = 0;
+unsigned long ultimoMilissegundo = 0;
+
 bool estadoAmarelo = false;
 bool conectado = false;
+bool cicloIniciado = false;
+int estadoSemaforo = 0;
 
 void mqttCallback(char* topic, byte* payload, unsigned int length) {
     String mensagem;
@@ -28,19 +32,16 @@ void mqttCallback(char* topic, byte* payload, unsigned int length) {
     }
 
     int estado = mensagem.toInt();
+    if (!cicloIniciado) {
+        cicloIniciado = true;
+    }
 
     if (estado == 0) {
-        digitalWrite(LED_R, LOW);
-        digitalWrite(LED_Y, LOW);
-        digitalWrite(LED_G, HIGH);
+        estadoSemaforo = 2;
     } else if (estado == 1) {
-        digitalWrite(LED_R, LOW);
-        digitalWrite(LED_Y, HIGH);
-        digitalWrite(LED_G, LOW);
+        estadoSemaforo = 1;
     } else if (estado == 2) {
-        digitalWrite(LED_R, HIGH);
-        digitalWrite(LED_Y, LOW);
-        digitalWrite(LED_G, LOW);
+        estadoSemaforo = 0;
     }
 }
 
@@ -81,11 +82,48 @@ void piscarAmareloFalha() {
     digitalWrite(LED_G, LOW);
 }
 
+void executarCicloSemaforo() {
+    unsigned long millisAtual = millis();
+
+    switch (estadoSemaforo) {
+        case 0:
+            if (millisAtual - ultimoMilissegundo >= 6000) {
+                ultimoMilissegundo = millisAtual;
+                estadoSemaforo = 1;
+            }
+            digitalWrite(LED_R, HIGH);
+            digitalWrite(LED_Y, LOW);
+            digitalWrite(LED_G, LOW);
+            break;
+
+        case 1:
+            if (millisAtual - ultimoMilissegundo >= 2000) {
+                ultimoMilissegundo = millisAtual;
+                estadoSemaforo = 2;
+            }
+            digitalWrite(LED_R, LOW);
+            digitalWrite(LED_Y, HIGH);
+            digitalWrite(LED_G, LOW);
+            break;
+
+        case 2:
+            if (millisAtual - ultimoMilissegundo >= 6000) {
+                ultimoMilissegundo = millisAtual;
+                estadoSemaforo = 0;
+            }
+            digitalWrite(LED_R, LOW);
+            digitalWrite(LED_Y, LOW);
+            digitalWrite(LED_G, HIGH);
+            break;
+    }
+}
+
 void setup() {
     pinMode(LED_R, OUTPUT);
     pinMode(LED_Y, OUTPUT);
     pinMode(LED_G, OUTPUT);
     Serial.begin(115200);
+
     WiFi.begin(SSID, PASSWORD);
     client.setServer(MQTT_SERVER, 1883);
     client.setCallback(mqttCallback);
@@ -102,7 +140,9 @@ void loop() {
         conectado = false;
     }
 
-    if (!conectado) {
+    if (!conectado || !cicloIniciado) {
         piscarAmareloFalha();
+    } else {
+        executarCicloSemaforo();
     }
 }
